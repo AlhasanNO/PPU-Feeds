586d15fdf5029b23a346b3fff01faecbaf964787
AlhasanNO <aalhassann@gmail.com>
2024-12-21 18:17:18 +0200

    HomeCourseCard

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: use_key_in_widget_constructors, use_build_context_synchronously, prefer_const_constructors, avoid_print, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:ppu_feeds/custom_widgets/homecourse_card.dart';
 import 'package:ppu_feeds/pages/feeds_screen.dart';
 import 'package:ppu_feeds/pages/home_screen.dart';
 import 'package:ppu_feeds/pages/login_screen.dart';
